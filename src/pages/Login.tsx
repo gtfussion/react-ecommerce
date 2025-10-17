14d9e06fb019d6e37a5c523f73d5c8bc9ae759e5
Prashant Shrestha <axiosprashant@gmail.com>
2025-10-17 14:40:58 +0545

    login page changes

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,7 +6,11 @@ export const Login = () => {
   const isLogin = useLoginStore((state) => state.isLogin);
 
   return (
-    <div className="relative shadow-[0_0_40px_10px_rgba(217,119,6,0.4)]  max-w-5xl mx-auto mt-[8rem]  min-h-[550px] max-h-[550px] h-full border-2 border-amber-600 bg-amber-700 rounded-md p-8 overflow-hidden transition-all duration-700 ease-in-out">
+    <div
+      className={`relative shadow-[0_0_40px_10px_rgba(217,119,6,0.4)]  max-w-5xl mx-auto mt-[8rem]  min-h-[550px] max-h-[550px] h-full border-2 border-amber-600 bg-linear-to-br ${
+        !isLogin ? "from-amber-700 to-black" : "from-black to-amber-700"
+      } rounded-md p-8 overflow-hidden transition-all duration-700 ease-in-out`}
+    >
       <div
         className={`absolute transition-all duration-700 ease-in-out top-0  w-[200%] h-[200%] bg-black   
           ${
